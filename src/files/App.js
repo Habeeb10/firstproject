e232c10b28740f88936ed769ce0de0a9e15432d7
habeeb10 <79145763+habeeb10@users.noreply.github.com>
2021-02-19 13:41:09 +0100

    Initial commit

---
+++ src/files/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [name, setname] = useState("");
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>
+        <button onClick={() => setname("Eyo")}>SetName</button>
+        {name.length && <h1>yes...length is tru</h1>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
