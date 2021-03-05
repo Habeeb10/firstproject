8726fa178356f6f431b4ded5066d1825d64e0269
habeeb10 <79145763+habeeb10@users.noreply.github.com>
2021-03-05 11:31:12 +0100

    completed header

---
+++ src/files/component/header.js
@@ -0,0 +1,17 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>Open Enterprise</h2>
+      <div className="textList">
+        <h6>why open enterprise</h6>
+        <h6>features</h6>
+        <h6>contribute</h6>
+        <h6>request early access</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
